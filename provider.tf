1c09a0200c5c2a1e52f5f4e664ddcd486c65df27
Dhirendra <dhirendra@192.168.1.5>
2023-09-14 20:43:54 +0530

    databricks

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
+    databricks = {
+      source = "databricks/databricks"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+provider "databricks" {}
